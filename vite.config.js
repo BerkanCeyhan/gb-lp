3f8b18cac1b6103e3e6bce7250772be98059a337
Berkan Ceyhan <berkanceyhan@gmail.com>
2026-03-28 23:00:38 +0100

    Deploy: custom domain start.brustbizeps.de, base path set to root
    - Add public/CNAME with start.brustbizeps.de
    - Set vite base to '/' for custom domain root serving
    - Update deploy script: remove --dest 1 subfolder
    - Update homepage to start.brustbizeps.de

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/gb-lp/1/',
+  base: '/',
   build: {
     rollupOptions: {
       output: {
